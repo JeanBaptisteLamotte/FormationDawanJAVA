714c25a8193e8ade4985df3aaaa71e764357328c
JBL88 <jblamotte1104@gmail.com>
2022-12-15 11:07:52 +0100

    "test commit"

---
+++ 02-ConceptsObjets/src/TP2/Consultant.java
@@ -0,0 +1,9 @@
+package TP2;
+
+public class Consultant extends Salarie{
+
+	public Consultant(String nom, String prenom, int age, double salaire, boolean presence) {
+		
+	}
+
+}
---
+++ 02-ConceptsObjets/src/TP2/Salarie.java
@@ -0,0 +1,13 @@
+package TP2;
+
+public abstract class Salarie {
+		
+		private String nom;
+		private String prenom;
+		private int age;
+		private int salaire;
+		private boolean presence;
+
+		
+		
+}
